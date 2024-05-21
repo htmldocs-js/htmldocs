f98cd9bd97d2190e69ef8ad28b81b7c78f9c87be
Kelvin Zhang <me@kelvinzhang.ca>
2024-05-21 18:26:39 -0400

    get monorepo working

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import React from "react";
 import { Document, Head, Page, Spacer, useValue } from "@htmldocs/react";
 import clsx from "clsx";
--- packages/webpack/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  external: ["uglify-js", "@swc/core", "esbuild", "./processChild", "./minify", "watchpack"],
-});
-
