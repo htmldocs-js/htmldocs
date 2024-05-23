9433ca7837a0201b867ea5a6f6040d6aff758c6d
jun-zhg <admin@htmldocs.com>
2024-05-23 17:42:35 -0400

    get css working

--- packages/react/components/Document.tsx
+++ packages/react/components/Document.tsx
@@ -1,35 +1,30 @@
-import { useEffect } from "react";
 import { DocumentContextProvider } from "./DocumentContext";
 
 const sizes = ["A3", "A4", "A5", "letter", "legal"];
 const orientations = ["portrait", "landscape"];
 
 interface Props {
-    size: typeof sizes[number];
-    orientation: typeof orientations[number];
-    margin?: React.CSSProperties["margin"];
-    children: React.ReactNode;
-};
+  size: (typeof sizes)[number];
+  orientation: (typeof orientations)[number];
+  margin?: React.CSSProperties["margin"];
+  children: React.ReactNode;
+}
 
 const Document: React.FC<Props> = ({ size, orientation, margin, children }) => {
-    useEffect(() => {
-        const updateBodyClass = (size: typeof sizes[number], orientation: typeof orientations[number]) => {
-            const body = document.body;
-
-            body.classList.remove(...sizes, ...orientations);
-            body.classList.add(size, orientation);
-        };
-
-        updateBodyClass(size, orientation);
-    }, [size, orientation]);
-
-    return (
-        <DocumentContextProvider defaults={{ pageSize: size, orientation, margin }}>
-            <main style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                {children}
-            </main>
-        </DocumentContextProvider>
-    );
+  return (
+    <DocumentContextProvider defaults={{ pageSize: size, orientation, margin }}>
+      <main
+        className={`${size} ${orientation}`}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {children}
+      </main>
+    </DocumentContextProvider>
+  );
 };
 
 export default Document;
