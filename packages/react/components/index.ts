9d65e507e94ff3209810408151dc7fab1479d680
Kelvin Zhang <me@kelvinzhang.ca>
2025-01-18 23:04:11 -0500

    add missing export

--- packages/react/components/index.ts
+++ packages/react/components/index.ts
@@ -3,3 +3,4 @@ export { default as Page } from "./Page";
 export { default as Head } from "./Head";
 export { default as Spacer } from "./Spacer";
 export { default as Footer } from "./Footer";
+export { default as MarginBox } from "./MarginBox";
