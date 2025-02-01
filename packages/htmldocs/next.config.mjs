ff08402a83b8a8d33d3f4bdc8be5f790cf8e619c
jun <admin@htmldocs.com>
2025-02-01 16:37:42 -0500

    update why

--- packages/htmldocs/next.config.mjs
+++ packages/htmldocs/next.config.mjs
@@ -9,4 +9,8 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = (await import('@next/bundle-analyzer')).default({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default withBundleAnalyzer(nextConfig);
